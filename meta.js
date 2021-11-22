67a790e28c34a61a7f3626037bbecc6f60e4c1c7
Fabian <fabian@fmbb.se>
2021-11-22 22:41:26 +0100

    meta: defs

--- meta.js
+++ meta.js
@@ -7,6 +7,7 @@ function pluginMetaPrimitives(_, _, primitives) {
         read: ([s]) => read(s.slice(1, -1)),
         eval: ([ast], env) => eval(ast, env.push()), // N.B cannot update env
         print: ([ast]) => print(ast),
+        defs: (_, env) => env.names(),
     });
 
 }
