fd2c61e884ec2cf3519531fcc03e26dca255fcea
Fabian <fabian@fmbb.se>
2021-11-22 21:26:48 +0100

    fix fn

--- meta.js
+++ meta.js
@@ -5,7 +5,7 @@ function pluginMetaPrimitives(_, _, primitives) {
 
     Object.assign(primitives, {
         read: ([s]) => read(s.slice(1, -1)),
-        eval: ([ast], env) => eval(ast, new Environment({}, env)), // N.B cannot update env
+        eval: ([ast], env) => eval(ast, env.push()), // N.B cannot update env
         print: ([ast]) => print(ast),
     });
 
